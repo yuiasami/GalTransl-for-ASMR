ba8d6e2816629ed1e7a8531ff96a70751caf3898
shinnpuru <89232015+shinnpuru@users.noreply.github.com>
2024-04-12 16:21:58 +0800

    支持srt输入

--- main.py
+++ main.py
@@ -5,23 +5,34 @@ print("欢迎使用GalTransl for ASMR！")
 
 print("您可以使用本程序将日语音频文件转换为字幕文件。")
 
-input_file = input("请输入音频文件路径（或拖拽文件到窗口）：").strip('"')
+input_file = input("请输入音频文件/SRT文件路径（或拖拽文件到窗口）：").strip('"')
 
-import os
-os.system("")
-model_size = BulletMenu(
-    "请选择语音识别模型大小:（默认为large-v3）", {'large-v3': "大模型，约5G显存", 'small':"小模型，约2G显存", 'medium':"中模型，约3G显存"}
-).run(0)
+if input_file.endswith('.srt'):
+    print("正在进行字幕转换...")
+    from srt2prompt import make_prompt
 
-print("正在进行语音识别...")
-from whisper2prompt import execute_asr
-output_file_path = execute_asr(
-    input_file  = input_file,
-    output_folder = 'sampleProject/gt_input',
-    model_size    = model_size,
-    language      = 'ja',
-    precision     = 'float16',
-)
+    import os
+    output_file_path = os.path.join('sampleProject/gt_input', os.path.basename(input_file).replace('.srt','.json'))
+
+    make_prompt(input_file, output_file_path)
+    print("字幕转换完成！")
+
+else:
+    import os
+    os.system("")
+    model_size = BulletMenu(
+        "请选择语音识别模型大小:（默认为large-v3）", {'large-v3': "大模型，约5G显存", 'small':"小模型，约2G显存", 'medium':"中模型，约3G显存"}
+    ).run(0)
+
+    print("正在进行语音识别...")
+    from whisper2prompt import execute_asr
+    output_file_path = execute_asr(
+        input_file  = input_file,
+        output_folder = 'sampleProject/gt_input',
+        model_size    = model_size,
+        language      = 'ja',
+        precision     = 'float16',
+    )
 
 
 project_dir, config_file_name = 'sampleProject', 'config.yaml'
---
+++ srt2prompt.py
@@ -0,0 +1,31 @@
+# convert srt file to prompt file
+
+import json, argparse
+from datetime import timedelta
+
+def make_prompt(input_file, output_file):
+    # read srt file
+    with open(input_file, encoding='utf-8') as f:
+        lines = f.readlines()
+    # parse srt file
+    data = []
+    for i in range(0, len(lines), 4):
+        start, end = lines[i+1].strip().split(" --> ")
+
+        # hh:mm:ss,ms to seconds
+        start = sum(x * int(t) for x, t in zip([3600, 60, 1, 0.001], start.replace(",",":").split(":")))
+
+        end = sum(x * int(t) for x, t in zip([3600, 60, 1, 0.001], end.replace(",",":").split(":")))
+
+        message = lines[i+2].strip()
+        data.append({"start": start, "end": end, "message": message})
+    # write prompt file
+    with open(output_file, 'w', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input_file", type=str, required=True)
+    parser.add_argument("-o","--output_file", type=str, required=True)
+    args = parser.parse_args()
+    make_prompt(args.input_file, args.output_file)
