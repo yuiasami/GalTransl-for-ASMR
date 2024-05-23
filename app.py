4549de4983de18bff920d0eca2b4e7978e8907b3
shinnpuru <89232015+shinnpuru@users.noreply.github.com>
2024-05-23 09:05:14 +0800

    add jp srt output

--- app.py
+++ app.py
@@ -64,11 +64,12 @@ def worker(input_file, model_size, translator, gpt_token, moonshot_token, sakura
     from prompt2srt import make_srt, make_lrc
     make_srt(output_file_path.replace('gt_input','gt_output'), input_file+'.srt')
     make_lrc(output_file_path.replace('gt_input','gt_output'), input_file+'.lrc')
+    make_srt(output_file_path, input_file+'.jp.srt')
     print("字幕文件生成完成！")
     print("输入输出缓存地址为：", os.path.dirname(input_file))
-    return input_file+'.srt', input_file+'.lrc'
+    return input_file+'.srt', input_file+'.lrc', input_file+'.jp.srt'
 
-def cleaner(input_file, output_srt, output_lrc):
+def cleaner(input_file, output_srt, output_lrc, output_jp_srt):
     print("正在清理输入...")
     if input_file:
         os.remove(input_file)
@@ -81,6 +82,8 @@ def cleaner(input_file, output_srt, output_lrc):
         os.remove(output_srt)
     if output_lrc:
         os.remove(output_lrc)
+    if output_jp_srt:
+        os.remove(output_jp_srt)
 
 with gr.Blocks() as demo:
     gr.Markdown("# 欢迎使用GalTransl for ASMR！")
@@ -103,11 +106,12 @@ with gr.Blocks() as demo:
     proxy_address = gr.Textbox(label="7. 请输入翻译引擎代理地址", placeholder="留空为不使用代理")
 
     run = gr.Button("8. 运行（状态详情请见命令行）")
-    output_srt = gr.File(label="9. 字幕文件(SRT)")
-    output_lrc = gr.File(label="10. 字幕文件(LRC)")
-    clean = gr.Button("11.清空输入输出缓存（请在使用完成后点击）")
+    output_srt = gr.File(label="9. 字幕文件(SRT)", interactive=False)
+    output_lrc = gr.File(label="10. 字幕文件(LRC)", interactive=False)
+    output_jp_srt = gr.File(label="11. 日语字幕文件(SRT)", interactive=False)
+    clean = gr.Button("12.清空输入输出缓存（请在使用完成后点击）")
 
-    run.click(worker, inputs=[input_file, model_size, translator, gpt_token, moonshot_token, sakura_address, proxy_address], outputs=[output_srt, output_lrc], queue=True)
-    clean.click(cleaner, inputs=[input_file, output_srt, output_lrc])
+    run.click(worker, inputs=[input_file, model_size, translator, gpt_token, moonshot_token, sakura_address, proxy_address], outputs=[output_srt, output_lrc, output_jp_srt], queue=True)
+    clean.click(cleaner, inputs=[input_file, output_srt, output_lrc, output_jp_srt])
 
 demo.queue().launch(inbrowser=True, server_name='0.0.0.0')
