89769c1eedc651ba006b13864939f545e5495823
昕蒲 <89232015+shinnpuru@users.noreply.github.com>
2024-09-21 13:41:50 +0800

    support multiple choice

--- app.py
+++ app.py
@@ -77,99 +77,108 @@ def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address
         title = re.sub(r'\s+', ' ', title)
         input_file = f'{title}.mp4'
 
-    if input_file.endswith('.srt'):
-        from srt2prompt import make_prompt
-        print("正在进行字幕转换...")
-        import os
-        os.makedirs('sampleProject/gt_input', exist_ok=True)
-        output_file_path = os.path.join('sampleProject/gt_input', os.path.basename(input_file).replace('.srt','.json'))
-
-        make_prompt(input_file, output_file_path)
-        print("字幕转换完成！")
+    if type(input_file) == str:
+        input_files = [input_file]
     else:
-        import os
-        os.makedirs('sampleProject/cache', exist_ok=True)
-        if os.path.exists(os.path.join('sampleProject/cache', os.path.basename(input_file))):
-            os.remove(os.path.join('sampleProject/cache', os.path.basename(input_file)))
-        input_file = shutil.move(input_file, 'sampleProject/cache/')
-        print("正在进行语音识别...")
-        from whisper2prompt import execute_asr
-        output_file_path = execute_asr(
-            input_file  = input_file,
-            output_folder = 'sampleProject/gt_input',
-            model_size    = model_size,
-            language      = 'ja',
-            precision     = 'float16',
-        )
-        print("语音识别完成！")
+        input_files = input_file
 
-    print("正在进行翻译配置...")
-    with open('sampleProject/config.yaml', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
+    for input_file in input_files:
+        print("-"*50)
+        print("当前处理文件：", input_file)
+        if input_file.endswith('.srt'):
+            from srt2prompt import make_prompt
+            print("正在进行字幕转换...")
+            import os
+            os.makedirs('sampleProject/gt_input', exist_ok=True)
+            output_file_path = os.path.join('sampleProject/gt_input', os.path.basename(input_file).replace('.srt','.json'))
 
-    for idx, line in enumerate(lines):
-        if 'gpt' in translator and gpt_token:
-            if 'GPT35' in line:
-                lines[idx+2] = f"      - token: {gpt_token}\n"
-                lines[idx+6] = f"    defaultEndpoint: https://api.openai.com\n"
-                lines[idx+7] = f'    rewriteModelName: ""\n'
-            if 'GPT4' in line:
-                lines[idx+2] = f"      - token: {gpt_token}\n"
-        if 'moonshot' in translator and gpt_token:
-            if 'GPT35' in line:
-                lines[idx+4] = f"      - token: {gpt_token}\n"
-                lines[idx+6] = f"    defaultEndpoint: https://api.moonshot.cn\n"
-                lines[idx+7] = f'    rewriteModelName: "moonshot-v1-8k"\n'
-        if 'qwen' in translator and gpt_token:
-            if 'GPT35' in line:
-                lines[idx+4] = f"      - token: {gpt_token}\n"
-                lines[idx+6] = f"    defaultEndpoint: https://dashscope.aliyuncs.com/compatible-mode\n"
-                lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
-        if 'glm' in translator and gpt_token:
-            if 'GPT35' in line:
-                lines[idx+4] = f"      - token: {gpt_token}\n"
-                lines[idx+6] = f"    defaultEndpoint: https://open.bigmodel.cn/api/paas\n"
-                lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
-        if 'abab' in translator and gpt_token:
-            if 'GPT35' in line:
-                lines[idx+4] = f"      - token: {gpt_token}\n"
-                lines[idx+6] = f"    defaultEndpoint: https://api.minimax.chat\n"
-                lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
-        if ('sakura' in translator or 'index' in translator or 'Galtransl' in translator) and sakura_address:
-            if 'Sakura' in line:
-                lines[idx+1] = f"    endpoint: {sakura_address}\n"
-        if proxy_address:
-            if 'proxy' in line:
-                lines[idx+1] = f"  enableProxy: true\n"
-                lines[idx+3] = f"    - address: {proxy_address}\n"
+            make_prompt(input_file, output_file_path)
+            print("字幕转换完成！")
         else:
-            if 'proxy' in line:
-                lines[idx+1] = f"  enableProxy: false\n"
+            import os
+            os.makedirs('sampleProject/cache', exist_ok=True)
+            if os.path.exists(os.path.join('sampleProject/cache', os.path.basename(input_file))):
+                os.remove(os.path.join('sampleProject/cache', os.path.basename(input_file)))
+            input_file = shutil.move(input_file, 'sampleProject/cache/')
+            print("正在进行语音识别...")
+            from whisper2prompt import execute_asr
+            output_file_path = execute_asr(
+                input_file  = input_file,
+                output_folder = 'sampleProject/gt_input',
+                model_size    = model_size,
+                language      = 'ja',
+                precision     = 'float16',
+            )
+            print("语音识别完成！")
 
-    if 'moonshot' in translator or 'qwen' in translator or 'glm' in translator or 'abab' in translator:
-        translator = 'gpt35-0613'
-    
-    if 'index' in translator:
-        translator = 'sakura-009'
+        print("正在进行翻译配置...")
+        with open('sampleProject/config.yaml', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
 
-    if 'Galtransl' in translator:
-        translator = 'sakura-010'
+        for idx, line in enumerate(lines):
+            if 'gpt' in translator and gpt_token:
+                if 'GPT35' in line:
+                    lines[idx+2] = f"      - token: {gpt_token}\n"
+                    lines[idx+6] = f"    defaultEndpoint: https://api.openai.com\n"
+                    lines[idx+7] = f'    rewriteModelName: ""\n'
+                if 'GPT4' in line:
+                    lines[idx+2] = f"      - token: {gpt_token}\n"
+            if 'moonshot' in translator and gpt_token:
+                if 'GPT35' in line:
+                    lines[idx+4] = f"      - token: {gpt_token}\n"
+                    lines[idx+6] = f"    defaultEndpoint: https://api.moonshot.cn\n"
+                    lines[idx+7] = f'    rewriteModelName: "moonshot-v1-8k"\n'
+            if 'qwen' in translator and gpt_token:
+                if 'GPT35' in line:
+                    lines[idx+4] = f"      - token: {gpt_token}\n"
+                    lines[idx+6] = f"    defaultEndpoint: https://dashscope.aliyuncs.com/compatible-mode\n"
+                    lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
+            if 'glm' in translator and gpt_token:
+                if 'GPT35' in line:
+                    lines[idx+4] = f"      - token: {gpt_token}\n"
+                    lines[idx+6] = f"    defaultEndpoint: https://open.bigmodel.cn/api/paas\n"
+                    lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
+            if 'abab' in translator and gpt_token:
+                if 'GPT35' in line:
+                    lines[idx+4] = f"      - token: {gpt_token}\n"
+                    lines[idx+6] = f"    defaultEndpoint: https://api.minimax.chat\n"
+                    lines[idx+7] = f'    rewriteModelName: "{translator}"\n'
+            if ('sakura' in translator or 'index' in translator or 'Galtransl' in translator) and sakura_address:
+                if 'Sakura' in line:
+                    lines[idx+1] = f"    endpoint: {sakura_address}\n"
+            if proxy_address:
+                if 'proxy' in line:
+                    lines[idx+1] = f"  enableProxy: true\n"
+                    lines[idx+3] = f"    - address: {proxy_address}\n"
+            else:
+                if 'proxy' in line:
+                    lines[idx+1] = f"  enableProxy: false\n"
 
-    with open('sampleProject/config.yaml', 'w', encoding='utf-8') as f:
-        f.writelines(lines)
+        if 'moonshot' in translator or 'qwen' in translator or 'glm' in translator or 'abab' in translator:
+            translator = 'gpt35-0613'
+        
+        if 'index' in translator:
+            translator = 'sakura-009'
 
-    print("正在进行翻译...")
-    from GalTransl.__main__ import worker
-    worker('sampleProject', 'config.yaml', translator, show_banner=False)
+        if 'Galtransl' in translator:
+            translator = 'sakura-010'
 
-    print("正在生成字幕文件...")
-    from prompt2srt import make_srt, make_lrc
-    make_srt(output_file_path.replace('gt_input','gt_output'), input_file+'.srt')
-    make_lrc(output_file_path.replace('gt_input','gt_output'), input_file+'.lrc')
-    make_srt(output_file_path, input_file+'.jp.srt')
-    print("字幕文件生成完成！")
-    print("缓存地址为：", input_file)
-    return input_file+'.srt', input_file+'.lrc', input_file+'.jp.srt', input_file
+        with open('sampleProject/config.yaml', 'w', encoding='utf-8') as f:
+            f.writelines(lines)
+
+        print("正在进行翻译...")
+        from GalTransl.__main__ import worker
+        worker('sampleProject', 'config.yaml', translator, show_banner=False)
+
+        print("正在生成字幕文件...")
+        from prompt2srt import make_srt, make_lrc
+        make_srt(output_file_path.replace('gt_input','gt_output'), input_file+'.srt')
+        make_lrc(output_file_path.replace('gt_input','gt_output'), input_file+'.lrc')
+        make_srt(output_file_path, input_file+'.jp.srt')
+        print("字幕文件生成完成！")
+        print("缓存地址为：", input_file)
+
+    os.startfile(os.path.join(os.getcwd(),'sampleProject/cache'))
 
 def cleaner():
     print("正在清理中间文件...")
@@ -186,7 +195,7 @@ def cleaner():
 with gr.Blocks() as demo:
     gr.Markdown("# 欢迎使用GalTransl for ASMR！")
     gr.Markdown("您可以使用本程序将日语音视频文件/字幕文件转换为中文字幕文件。")
-    input_file = gr.File(label="1. 请选择音视频文件/SRT文件（或拖拽文件到窗口）")
+    input_file = gr.Files(label="1. 请选择音视频文件/SRT文件（或拖拽文件到窗口，可多选）")
     yt_url = gr.Textbox(label="输入YouTube视频链接（包含youtu.be或者youtube.com）或者Bilibili的BV号进行下载。", placeholder="https://www.youtube.com/watch?v=...")
     model_size = gr.Radio(
         label="2. 请选择语音识别模型大小:",
@@ -208,15 +217,10 @@ with gr.Blocks() as demo:
             after_dict = gr.Textbox(label="输出替换字典（中文到中文）", placeholder="中文\t中文\n中文\t中文")
 
 
-    run = gr.Button("9. 运行（状态详情请见命令行）")
-    with gr.Row():
-        output_srt = gr.File(label="字幕文件(SRT)", interactive=False)
-        output_lrc = gr.File(label="字幕文件(LRC)", interactive=False)
-        output_jp_srt = gr.File(label="日语字幕文件(SRT)", interactive=False)
-        output_mp4 = gr.File(label="视频/音频文件", interactive=False)
-    clean = gr.Button("10.清空输入输出缓存（请在使用完成后点击）")
+    run = gr.Button("9. 运行（状态详情请见命令行，完成后打开输出文件夹）")
+    clean = gr.Button("10.清空输入输出缓存（请在保存完成后点击）")
 
-    run.click(worker, inputs=[input_file, yt_url, model_size, translator, gpt_token, sakura_address, proxy_address, before_dict, gpt_dict, after_dict], outputs=[output_srt, output_lrc, output_jp_srt, output_mp4], queue=True)
+    run.click(worker, inputs=[input_file, yt_url, model_size, translator, gpt_token, sakura_address, proxy_address, before_dict, gpt_dict, after_dict], queue=True)
     clean.click(cleaner, inputs=[])
 
 demo.queue().launch(inbrowser=True, server_name='0.0.0.0')
