9c2936d3291d7b791e5285cb921861a8601ea338
昕蒲 <89232015+shinnpuru@users.noreply.github.com>
2024-09-29 21:16:57 +0800

    support multilang multiselect

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ TRANSLATOR_SUPPORTED = [
     "abab6.5s-chat",
 ]
 
-def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address, proxy_address, before_dict, gpt_dict, after_dict):
+def worker(input_files, yt_url, model_size, translator, gpt_token, sakura_address, proxy_address, before_dict, gpt_dict, after_dict):
     print("正在初始化项目文件夹...")
     if before_dict:
         with open('sampleProject/项目字典_译前.txt', 'w', encoding='utf-8') as f:
@@ -45,44 +45,45 @@ def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address
         if os.path.exists('sampleProject/项目字典_译后.txt'):
             os.remove('sampleProject/项目字典_译后.txt')
 
-    if yt_url and ('youtu.be' in yt_url or 'youtube.com' in yt_url):
-        from yt_dlp import YoutubeDL
-        import os
-        if os.path.exists('sampleProject/YoutubeDL.webm'):
-            os.remove('sampleProject/YoutubeDL.webm')
-        with YoutubeDL({'proxy': proxy_address,'outtmpl': 'sampleProject/YoutubeDL.webm'}) as ydl:
-            print("正在下载视频...")
-            results = ydl.download([yt_url])
-            print("视频下载完成！")
-        input_file = 'sampleProject/YoutubeDL.webm'
+    if not input_files:
+        input_files = []
 
-    elif yt_url and 'BV' in yt_url:
-        from bilibili_dl.bilibili_dl.Video import Video
-        from bilibili_dl.bilibili_dl.downloader import download
-        from bilibili_dl.bilibili_dl.utils import send_request
-        from bilibili_dl.bilibili_dl.constants import URL_VIDEO_INFO
-        print("正在下载视频...")
-        res = send_request(URL_VIDEO_INFO, params={'bvid': yt_url})
-        download([Video(
-            bvid=res['bvid'],
-            cid=res['cid'] if res['videos'] == 1 else res['pages'][0]['cid'],
-            title=res['title'] if res['videos'] == 1 else res['pages'][0]['part'],
-            up_name=res['owner']['name'],
-            cover_url=res['pic'] if res['videos'] == 1 else res['pages'][0]['pic'],
-        )], False)
-        print("视频下载完成！")
-        import re
-        title = res['title'] if res['videos'] == 1 else res['pages'][0]['part']
-        title = re.sub(r'[.:?/\\]', ' ', title).strip()
-        title = re.sub(r'\s+', ' ', title)
-        input_file = f'{title}.mp4'
-
-    if type(input_file) == str:
-        input_files = [input_file]
-    else:
-        input_files = input_file
+    if yt_url:
+        input_files.extend(yt_url.split('\n'))
 
     for input_file in input_files:
+        if not os.path.exists(input_file) and ('youtu.be' in input_file or 'youtube.com' in input_file):
+            from yt_dlp import YoutubeDL
+            import os
+            if os.path.exists('sampleProject/YoutubeDL.webm'):
+                os.remove('sampleProject/YoutubeDL.webm')
+            with YoutubeDL({'proxy': proxy_address,'outtmpl': 'sampleProject/YoutubeDL.webm'}) as ydl:
+                print("正在下载视频...")
+                results = ydl.download([input_file])
+                print("视频下载完成！")
+            input_file = 'sampleProject/YoutubeDL.webm'
+
+        elif not os.path.exists(input_file) and 'BV' in yt_url:
+            from bilibili_dl.bilibili_dl.Video import Video
+            from bilibili_dl.bilibili_dl.downloader import download
+            from bilibili_dl.bilibili_dl.utils import send_request
+            from bilibili_dl.bilibili_dl.constants import URL_VIDEO_INFO
+            print("正在下载视频...")
+            res = send_request(URL_VIDEO_INFO, params={'bvid': input_file})
+            download([Video(
+                bvid=res['bvid'],
+                cid=res['cid'] if res['videos'] == 1 else res['pages'][0]['cid'],
+                title=res['title'] if res['videos'] == 1 else res['pages'][0]['part'],
+                up_name=res['owner']['name'],
+                cover_url=res['pic'] if res['videos'] == 1 else res['pages'][0]['pic'],
+            )], False)
+            print("视频下载完成！")
+            import re
+            title = res['title'] if res['videos'] == 1 else res['pages'][0]['part']
+            title = re.sub(r'[.:?/\\]', ' ', title).strip()
+            title = re.sub(r'\s+', ' ', title)
+            input_file = f'{title}.mp4'
+
         print("-"*50)
         print("当前处理文件：", input_file)
         import os
@@ -91,6 +92,7 @@ def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address
             os.remove(os.path.join('sampleProject/cache', os.path.basename(input_file)))
         input_file = shutil.move(input_file, 'sampleProject/cache/')
 
+        from prompt2srt import make_srt, make_lrc
         if input_file.endswith('.srt'):
             from srt2prompt import make_prompt
             print("正在进行字幕转换...")
@@ -106,10 +108,15 @@ def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address
                 input_file  = input_file,
                 output_folder = 'sampleProject/gt_input',
                 model_size    = model_size,
-                language      = 'ja',
+                language      = 'auto',
                 precision     = 'float16',
             )
             print("语音识别完成！")
+            make_srt(output_file_path, input_file+'.raw.srt')
+
+        if translator is None:
+            print("翻译器未选择，跳过翻译步骤...")
+            continue
 
         print("正在进行翻译配置...")
         with open('sampleProject/config.yaml', 'r', encoding='utf-8') as f:
@@ -171,10 +178,8 @@ def worker(input_file, yt_url, model_size, translator, gpt_token, sakura_address
         worker('sampleProject', 'config.yaml', translator, show_banner=False)
 
         print("正在生成字幕文件...")
-        from prompt2srt import make_srt, make_lrc
         make_srt(output_file_path.replace('gt_input','gt_output'), input_file+'.srt')
         make_lrc(output_file_path.replace('gt_input','gt_output'), input_file+'.lrc')
-        make_srt(output_file_path, input_file+'.jp.srt')
         print("字幕文件生成完成！")
         print("缓存地址为：", input_file)
 
@@ -195,30 +200,31 @@ def cleaner():
 with gr.Blocks() as demo:
     gr.Markdown("# 欢迎使用GalTransl for ASMR！")
     gr.Markdown("您可以使用本程序将日语音视频文件/字幕文件转换为中文字幕文件。")
-    input_file = gr.Files(label="1. 请选择音视频文件/SRT文件（或拖拽文件到窗口，可多选）")
-    yt_url = gr.Textbox(label="输入YouTube视频链接（包含youtu.be或者youtube.com）或者Bilibili的BV号进行下载。", placeholder="https://www.youtube.com/watch?v=...")
+    with gr.Accordion("1. 选择输入", open=True):
+        input_file = gr.Files(label="请选择音视频文件/SRT文件，或拖拽文件到窗口，可多选。")
+        yt_url = gr.Textbox(label="或者输入YouTube视频链接（包含youtu.be或者youtube.com）或者Bilibili的BV号进行下载，空行区分不同视频。", placeholder="https://www.youtube.com/watch?v=...\nBV1Lxt5e8EJF")
+        proxy_address = gr.Textbox(label="请输入网络访问代理地址（可选）。", placeholder="例如：http://127.0.0.1:7890，留空为不使用代理")
     model_size = gr.Radio(
         label="2. 请选择语音识别模型大小:",
         choices=['small', 'medium', 'large-v3',],
         value='small'
     )
-    translator = gr.Radio(
-        label="3. 请选择翻译器：",
-        choices=TRANSLATOR_SUPPORTED,
-        value=TRANSLATOR_SUPPORTED[0]
-    )
-    gpt_token = gr.Textbox(label="4. 请输入 API Token (GPT, Moonshot, Qwen, GLM, MiniMax/abab)", placeholder="留空为使用上次配置的Token")
-    sakura_address = gr.Textbox(label="6. 请输入 API 地址 (Sakura, Index, Galtransl)", placeholder="例如：http://127.0.0.1:8080，留空为使用上次配置的地址")
-    proxy_address = gr.Textbox(label="7. 请输入翻译引擎和视频下载代理地址", placeholder="例如：http://127.0.0.1:7890，留空为不使用代理")
-    with gr.Accordion("8. 使用翻译字典（可选）", open=False):
+    with gr.Accordion("3. 配置翻译（可选）", open=False):
+        translator = gr.Radio(
+            label="请选择翻译器：",
+            choices=TRANSLATOR_SUPPORTED
+        )
+        gpt_token = gr.Textbox(label="请输入 API Token (如果选择GPT, Moonshot, Qwen, GLM, MiniMax/abab)", placeholder="留空为使用上次配置的Token")
+        sakura_address = gr.Textbox(label="请输入 API 地址 (如果选择Sakura, Index, Galtransl)", placeholder="例如：http://127.0.0.1:8080，留空为使用上次配置的地址")
+    with gr.Accordion("4. 翻译字典（可选）", open=False):
         with gr.Row():
             before_dict = gr.Textbox(label="输入替换字典（日文到日文）", placeholder="日文\t日文\n日文\t日文")
             gpt_dict = gr.Textbox(label="翻译替换字典（日文到中文，不支持sakura-009，Index）", placeholder="日文\t中文\n日文\t中文")
             after_dict = gr.Textbox(label="输出替换字典（中文到中文）", placeholder="中文\t中文\n中文\t中文")
 
 
-    run = gr.Button("9. 运行（状态详情请见命令行，完成后打开输出文件夹）")
-    clean = gr.Button("10.清空输入输出缓存（请在保存完成后点击）")
+    run = gr.Button("5. 运行（状态详情请见命令行，完成后打开输出文件夹）")
+    clean = gr.Button("6.清空输入输出缓存（请在保存完成后点击）")
 
     run.click(worker, inputs=[input_file, yt_url, model_size, translator, gpt_token, sakura_address, proxy_address, before_dict, gpt_dict, after_dict], queue=True)
     clean.click(cleaner, inputs=[])
